53cb2312ffeeb1366133eb7009cfd389e8965a5a
DarkEnvoy <devcpy@mail.ru>
2018-11-18 20:36:20 +0500

    Add some requirements Add some communication

--- src/main.py
+++ src/main.py
@@ -1,10 +1,56 @@
 import configparser
+import logging
+
+from telegram import Bot, ReplyKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def start(bot: Bot, update):
+    update.message.reply_text('Please choose:', reply_markup=ReplyKeyboardMarkup([['asdf']]))
+
+
+def button(bot: Bot, update: Update):
+    query = update.callback_query
+
+    bot.send_message(text="Selected option: {}".format(query.data),
+                     chat_id=query.message.chat_id)
+
+
+def on_message(bot: Bot, update: Update):
+    update.message.reply_text('Верно')
+
+
+def help(bot: Bot, update):
+    update.message.reply_text("Use /start to test this bot.")
+
+
+def error(bot: Bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
 
 
 def main():
     config = configparser.ConfigParser()
     config.read('../config.ini')
     api_key = config[config.default_section]['api_key']
+    proxy_url = config[config.default_section]['proxy_url']
+
+    updater = Updater(api_key,
+                      request_kwargs={'proxy_url': proxy_url})
+
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, on_message))
+    updater.dispatcher.add_handler(CommandHandler('help', help))
+    updater.dispatcher.add_error_handler(error)
+
+    updater.start_polling()
+
+    updater.idle()
 
 
 if __name__ == '__main__':
